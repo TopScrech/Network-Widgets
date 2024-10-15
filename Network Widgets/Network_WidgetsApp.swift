7f30d4fa1d042fccb852757e65db7c8988c12954
TopScrech <sergei_saliukov@icloud.com>
2024-10-15 12:51:34 +0200

    Initial Commit

---
+++ Network Widgets/Network_WidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Network_WidgetsApp.swift
+//  Network Widgets
+//
+//  Created by Sergei Saliukov on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Network_WidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
